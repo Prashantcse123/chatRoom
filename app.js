4b1a73ee8fab90f92311c93ea8424821d06410b3
Prashantcse123 <prashant.sharma.cse17@gmail.com>
2018-08-09 16:26:55 +0530

    two lines deleted

--- app.js
+++ app.js
@@ -95,8 +95,8 @@ app.use(passport.session());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
-app.use(express.json());
+
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
